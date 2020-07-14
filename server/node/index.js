af77e7928e9e70dfcfa23074998c85c404f77c0c
Marco Moltrasi <mastermoltra@gmail.com>
2020-07-14 09:52:31 +0200

    Added a basic node server with express

---
+++ server/node/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Express route handlers
+app.get('/', (req, res) => {
+    res.send('Hi');
+});
+
+app.listen(3000, (err) => {
+    console.log('Listening');
+});
